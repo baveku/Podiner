360a3160bca0541a3884f6d91bf12c9a8b28a6a7
bachvq <baveku@gmail.com>
2023-03-04 14:40:26 +0700

    fix ci

--- startup.ci.sh
+++ startup.ci.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-/opt/wait-for-it.sh postgres:5432
+/opt/wait-for-it.sh db:3306
 npm run migration:run
 npm run seed:run
-npm run start:prod > /dev/null 2>&1 &
+npm run start:prod >/dev/null 2>&1 &
 /opt/wait-for-it.sh maildev:1080
-/opt/wait-for-it.sh localhost:3000
+/opt/wait-for-it.sh localhost:9000
 npm run lint
 npm run test:e2e -- --runInBand
